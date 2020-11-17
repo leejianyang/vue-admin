3474f30052f2e128d295d71bbc1bbf377b235550
李剑扬 <lijianyang@3ekeji.cn>
2020-11-17 11:19:32 +0800

    init project

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # vue-admin
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+基于 vue2 和 Ant Design Vue 的后台管理系统练习项目。
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,18 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import BasicLayout from '.././layouts/BasicLayout'
 
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
+  mode: 'history',
+  base: process.env.BABEL_ENV,
+  routes: [
+    {
+      path: '/',
+      component: BasicLayout
+    }
+  ]
+})
 
-export default router;
+export default router
